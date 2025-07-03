11e4278bf79aefcac74c97c9aca3cffee55faec1
med <medbenattia09@gmail.com>
2025-07-03 22:14:54 +0100

    add server config

---
+++ microservices/server-config/src/test/java/com/att/server_config/ServerConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.att.server_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
