da339b510fa3dc91dfb7210f179b4c35e590ecbf
med <medbenattia09@gmail.com>
2025-07-06 00:51:04 +0100

    fix Gateway yml file

--- microservices/Invoice/src/main/java/com/example/Invoice/InvoiceConsumer.java
+++ microservices/Invoice/src/main/java/com/example/Invoice/InvoiceConsumer.java
@@ -4,16 +4,23 @@ package com.example.Invoice;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 public class InvoiceConsumer {
     private final InvoiceRepository repository;
 
+    public InvoiceConsumer(InvoiceRepository repository) {
+        this.repository = repository;
+    }
+
     @KafkaListener(topics = "booking-topic")
-    public void InvoiceConsumer(BookingBiLL bookingBiLL) {
+    public void InvoiceRegister(BookingBiLL bookingBiLL) {
 
         repository.save(
                 Invoice.builder()
-
+                        .registerDate(LocalDateTime.now())
+                        .bookingBiLL(bookingBiLL)
                         .build()
         );
 
